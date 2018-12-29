f8a5409d7952fc383d982767ab3220d808f70ea1
huoshan017 <huoshan017@hotmail.com>
2018-12-29 18:03:12 +0800

    11111

---
+++ php/test.php
@@ -0,0 +1,23 @@
+<?php
+include 'http_send.php';
+include 'format_gm_cmd.php';
+include 'config.php';
+
+
+$num=$_POST["Num"];
+$string=$_POST["String"];
+
+$cmd = array('StringValue'=>$string, 'NumValue'=>intval($num));
+$jd = json_encode($cmd);
+$bd = base64_encode($jd);
+$data = format_gmcmd(0, $bd, "test");
+$cfg = get_config();
+if ($cfg == null) {
+    echo "get cfg failed";
+    return;
+}
+sock_post(get_gm_url(), $data);
+//sleep(2);
+//header("Location: anounce.html");
+
+?>
