559555225a7904a13919ca977783ea7d524384ce
huoshan017 <huoshan017@hotmail.com>
2019-01-15 20:40:35 +0800

    11111

--- php/http_send.php
+++ php/http_send.php
@@ -1,43 +1,23 @@
 <?php
-/**
-  * 发送post请求
-  * @param string $url 请求地址
-  * @param array $post_data post键值对数据
-  * @return string
-  */
- function send_post($url, $post_data) {
-    $options = array(
-        'http' => array(
-             'method' => 'POST',
-             'header' => 'Content-type:application/x-www-form-urlencoded',
-             'content' => $post_data,
-             'timeout' => 5 // 超时时间（单位:s）
-        )
-    );
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
- 
+
+require_once 'third_party/Requests/library/Requests.php';
+
+Requests::register_autoloader();
+
+function RequestsPost($url, $query)
+{
+    $header = array('Content-type'=>'application/x-www-form-urlencoded');
+    $result = Requests::post($url, $header, $query);
+    //var_dump($result);
     return $result;
 }
 
-function sock_post($url, $query) 
-{ 
-    $info = parse_url($url); 
-    $fp = fsockopen($info['host'], $info["port"], $errno, $errstr, 3); 
-    $head = "POST ".$info['path']." HTTP/1.0\r\n"; 
-    $head .= "Host: ".$info['host']."\r\n"; 
-    $head .= "Referer: http://".$info['host'].$info['path']."\r\n"; 
-    $head .= "Content-type: application/x-www-form-urlencoded\r\n"; 
-    $head .= "Content-Length: ".strlen(trim($query))."\r\n"; 
-    $head .= "\r\n"; 
-    $head .= trim($query);
-    $write = fputs($fp, $head); 
-    //print_r(fgets($fp));
-    while (!feof($fp)) 
-    { 
-        $line = fgets($fp); 
-        echo $line."<br>"; 
-    } 
-} 
+function RequestsGet($url, $query)
+{
+    $header = array('Content-type'=>'application/x-www-form-urlencoded');
+    $result = Requests::get($url, $header);
+    //var_dump($result);
+    return $result;
+}
 
 ?>
