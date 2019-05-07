7fa0c4903d0e147999758292cc2a78f72feedd11
huoshan017 <huoshan017@hotmail.com>
2019-05-07 18:41:13 +0800

    11111

--- php/history.php
+++ php/history.php
@@ -41,14 +41,30 @@ foreach ($tab_headers as $k=>$v) {
 }
 echo '</tr>';
 
-$offset = $page * 30;
+$need_fetch = 30;
+$offset = $page * $need_fetch;
+$real_fetch = $need_fetch+1;
 $result;
 if ($account == '') {
-    $result = mysqli_query($dbc, "select * from history order by act_time desc limit $offset, 30;");
+    $result = mysqli_query($dbc, "select * from history order by act_time desc limit $offset, $real_fetch;");
 } else {
-    $result = mysqli_query($dbc, "select * from history where act_account='$account' order by act_time desc limit $offset, 30;");
+    $result = mysqli_query($dbc, "select * from history where act_account='$account' order by act_time desc limit $offset, $real_fetch;");
 }
+
+if ($account == "") {
+    echo '<label>当前账号: 所有</label><br>';
+} else {
+    echo '<label>当前账号: ' . $account . '</label><br>';
+}
+
+$display_page = $page + 1;
+echo '<label>当前页: ' . $display_page . '</label><br>';
+
+$cnt = 0;
 while ($row = mysqli_fetch_array($result)) {
+    if ($cnt > $need_fetch) {
+        break;
+    }
     $act_account = $row["act_account"];
     $id = $row["id"];
     $act_id = $row["act_id"];
@@ -57,6 +73,17 @@ while ($row = mysqli_fetch_array($result)) {
     $act_time = $row["act_time"];
     $detail = $row["detail"];
     echo '<tr><td>' . $act_account . '</td><td>' . $id . '</td><td>' . $act_id . '</td><td>' . $act_name . '</td><td>' . $act_result . '</td><td>' . $act_time . '</td><td>' . $detail . '</td></tr>';
+    $cnt += 1;
+}
+
+if ($page > 0) {
+    $prev_page = $page -1;
+    echo '<a href="javascript:SeeHistory(\'' . $account . '\', ' . $prev_page . ')"> 上一页 </a>';
+}
+
+if ($cnt > $need_fetch) {
+    $next_page = $page + 1;
+    echo '<a href="javascript:SeeHistory(\'' . $account . '\', ' . $next_page . ')"> 下一页 </a>';
 }
 
 ?>
